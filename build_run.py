c7754cb21c68a8d83f4fbeb678f7acf787a4d21a
michaelposada <michaelposada@mail.adelphi.edu>
2019-07-19 10:25:12 -0400

    Debian 7 works

--- build_run.py
+++ build_run.py
@@ -1,9 +1,10 @@
 ##Version Python 3.7.6
-
 import os
 
 print("I will now start to build these Images. Debian 7, Debian 8, Debian 9, Ubuntu 18.04 and Windows")
 
-os.system("cd Debian-7")
-os.system("docker build -t isa/debian7 .")
+os.chdir("Debian-7/")
+os.system("ls -a")
+print("I am now inside Debian-7")
+os.system("docker build -t isa/debian7 . ")
 os.system("cd ..")
