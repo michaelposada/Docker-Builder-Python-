39861c8b8261bce2a3d2ba0688403daf9e5cab6d
michaelposada <michaelposada@mail.adelphi.edu>
2019-08-07 11:15:38 -0400

    Working Linux Containers

--- Debian-8/run_build.sh
+++ Debian-8/run_build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # This bash script will be run with when the docker image generated from the
 # Dockerfile in this directory is run
-export http_proxy=http://10.0.137.23:8888
+export http_proxy=INSERT PROXY HERE
 export https_proxy=$http_proxy
-git config --global http.proxy http://10.0.137.23:8888
+git config --global http.proxy INSERT PROXY HERE
 git clone --single-branch --branch=feature-integrated-packager https://github.com/jwlodek/installSynApps
 cd installSynApps
 python3 installCLI.py -y -c addtlConfDirs/minConfigureLinux
